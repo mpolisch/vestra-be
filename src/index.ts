c4afce9de61cdb1cb80112ce8c1c692e5559e1e3
Matthew <mpolisch@uoguelph.ca>
2026-04-30 15:33:03 -0400

    created entryway, configured json files.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type {Request, Response} from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the backend server!');
+})
+
+const PORT: number = parseInt(process.env.PORT || '4000', 10);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
